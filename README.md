7c0a19ce4a88084499efe4958bf97e64cf5cc24c
Administrator <153437803@qq.com>
2020-11-11 14:30:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿
+参考文章(针对89a)
+https://blog.csdn.net/wzy198852/article/details/17266507
+
+源码路径
+https://www.androidos.net.cn/android/10.0.0_r6/xref/external/giflib
---
+++ app/src/main/java/com/kalu/gif/MainActivity.java
@@ -0,0 +1,100 @@
+package com.kalu.gif;
+
+import android.Manifest;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import lib.kalu.gif.GifManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mIvShow;
+    private GifManager mGifPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 666);
+        }
+        mIvShow = findViewById(R.id.iv_show);
+        mGifPlayer = new GifManager();
+        mGifPlayer.setOnGifListener(new GifManager.OnGifListener() {
+            @Override
+            public void start() {
+                Log.i("DMUI", "gif start");
+            }
+
+            @Override
+            public void draw(final Bitmap bitmap) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mIvShow.setImageBitmap(bitmap);
+                    }
+                });
+            }
+
+            @Override
+            public void end() {
+                Log.i("DMUI", "gif end");
+            }
+        });
+        findViewById(R.id.btn_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGifPlayer.assetPlay(false, MainActivity.this, "demo2.gif");
+//                // 外部目录play
+//                File file = new File(Environment.getExternalStorageDirectory(), "1/test.gif");
+//                if (file.exists()) {
+//                    mGifPlayer.storagePlay(false,file.getPath());
+//                } else {
+//                    Toast.makeText(MainActivity.this, "file not exists!!!", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        });
+        findViewById(R.id.btn_two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGifPlayer.pause();
+            }
+        });
+        findViewById(R.id.btn_three).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGifPlayer.resume();
+            }
+        });
+        findViewById(R.id.btn_four).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGifPlayer.stop();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGifPlayer.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        mGifPlayer.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mGifPlayer.destroy();
+        super.onDestroy();
+    }
+}
